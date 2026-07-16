0413a9415118c057267a71411df7d09e1d815f52
sourjyendu-barik <sourjyendubarik7798@gmail.com>
2026-07-16 16:59:47 +0530

    added suggestion route

---
+++ controllers/findUserController.js
@@ -0,0 +1,38 @@
+const { UserModel } = require("../models/user");
+
+const findUsersSuggestion = async (req, res) => {
+  try {
+    const { q } = req.query;
+    if (!q || q.trim() === "") {
+      return res
+        .status(200)
+        .json({ data: [], success: true, message: "No data provided" });
+    }
+    const escapeRegex = (text) => text.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+    const search = escapeRegex(q.trim());
+
+    const users = await UserModel.find({
+      email: {
+        $regex: `^${search}`,
+        $options: "i", // case-insensitive
+      },
+    })
+      .select("email -_id")
+      .limit(10)
+      .lean();
+
+    //extract emails
+    const emails = users.map((user) => user.email);
+    return res.status(200).json({ data: emails, success: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "server error while fetching emails",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { findUsersSuggestion };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { myData, logout } = require("../controllers/userConroller");
+const { findUsersSuggestion } = require("../controllers/findUserController");
 router.post("/logout", logout);
 router.get("/me", myData);
+router.get("/findEmail", findUsersSuggestion);
 module.exports = router;
